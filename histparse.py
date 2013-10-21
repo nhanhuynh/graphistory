26f3e7c2ce2ea2d674494bfc31a4fad62adb5f6b
David Harwood <dharwood@cecs.pdx.edu>
2013-10-20 21:47:20 -0700

    Chrome query needs more testing but think it works

--- histparse.py
+++ histparse.py
@@ -15,13 +15,16 @@ import os.path
 ###
 
 def hist_read(histloc):
-    #Chromium/Chrome and Firefox both use sqlite for their history, IE uses what would appear to be a binary blob, and Safari uses plist files for history; IE and Safari may not be supported by project deadline
+    #Chromium/Chrome and Firefox both use sqlite for their history, IE uses what would appear to be a binary file, and Safari uses plist files for history; IE and Safari may not be supported by project deadline
     histloc = os.path.abspath(os.path.expanduser(histloc))
-    conn = sqlite3.connect(histloc) #I think there's a better way to do this with os.path, but this'll do for now
-    count = conn.execute("SELECT COUNT(*) FROM sqlite_master WHERE tbl_name LIKE 'moz_%';").fetchall()[0][0]
+    conn = sqlite3.connect(histloc)
+    count_ff = conn.execute("SELECT COUNT(*) FROM sqlite_master WHERE tbl_name LIKE 'moz_%';").fetchall()[0][0]
+    count_ch = conn.execute("SELECT COUNT(*) FROM sqlite_master WHERE name = 'downloads_url_chains';").fetchall()[0][0] #this is not ideal, but Chromium doesn't have any easy branding in the history file
     #New history sqlite formats can be added into the following conditional
-    if count > 0:
+    if count_ff > 0:
         hist_data = conn.execute("SELECT fromurl.url, tourl.url FROM moz_places AS tourl, moz_historyvisits AS tohist, moz_places AS fromurl, moz_historyvisits AS fromhist WHERE tourl.id = tohist.place_id AND fromurl.id = fromhist.place_id AND tohist.id = fromhist.from_visit;").fetchall()
+    elif count_ch > 0:
+        hist_data = conn.execute("SELECT fromurl.url, tourl.url FROM urls AS fromurl, visits AS fromhist, urls AS tourl, visits AS tohist WHERE tourl.id = tohist.id AND fromurl.id = fromhist.id AND tohist.from_visit = fromhist.id;").fetchall()
     else:
         print("Invalid file. Exiting")
         sys.exit(1)
